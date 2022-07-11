c49d0816a51c6c929cfa17a0e00950dfc5c10e58
Елена <lena.lena1803@yandex.ru>
2022-07-12 00:48:48 +0300

    Каркас 12

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,43 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import {Route, Routes, Navigate, useNavigate} from 'react-router-dom';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import Main from './Main.js';
 import PopupWithForm from './PopupWithForm.js';
 import ImagePopup from './ImagePopup.js';
+import * as auth from '../utils/auth';
 import api from "../utils/api";
 import {CurrentUserContext} from '../contexts/CurrentUserContext'
 import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
 import AddPlacePopup from "./AddPlacePopup";
+import ProtectedRoute from "./ProtectedRoute";
+import Login from "./Login";
+import Register from "./Register";
+import InfoTooltip from "./InfoTooltip";
 
 
 function App() {
+    const history = useNavigate();
+    const initialData = {
+        email: ''
+    }
     const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
     const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
     const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
     const [selectedCard, setSelectedCard] = useState({isOpen: false});
     const [currentUser, setCurrentUser] = useState({'name': 'Жак', 'about': 'test', 'avatar': 'http://ya.ru'});
     const [cards, setCards] = useState([]);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [isAuthChecking, setIsAuthChecking] = useState(true);
+    const [isInfoTooltipPopupOpen, setIsInfoTooltipPopupOpen] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [data, setData] = useState(initialData);
 
+
+    const closePopup = () => {
+        setIsInfoTooltipPopupOpen(false)
+    }
 
     useEffect(() => {
         api.getAllNeededData()
@@ -104,20 +123,110 @@ function App() {
             .catch((err) => console.log(err))
     }
 
+    const handleRegister = ({password, email}) => {
+        return auth.register({password, email})
+            .then(res => {
+                if (!res || res.statusCode === 400) throw new Error(`Ошибка: ${res.message}`);
+                setIsInfoTooltipPopupOpen(true);
+                setIsSuccess(true);
+                history.push('/sign-in')
+                return res;
+            })
+            .catch(err => {
+                setIsInfoTooltipPopupOpen(true);
+                setIsSuccess(false);
+                return err;
+            })
+    }
+
+    const tokenCheck = useCallback(() => {
+        const jwt = localStorage.getItem('jwt');
+
+        if (jwt) {
+            setIsAuthChecking(true);
+            auth.getContent(jwt)
+                .then((res) => {
+                    if (res) {
+                        setLoggedIn(true);
+                        setData({
+                            email: res.data.email
+                        })
+                        history.push('/mesto');
+                    }
+                })
+                .catch(() => history.push('/sign-in'))
+                .finally(() => setIsAuthChecking(false))
+        } else {
+            setIsAuthChecking(false)
+        }
+    }, [history])
+
+    useEffect(() => {
+        tokenCheck();
+    }, [tokenCheck])
+
+    const handleLogin = ({password, email}) => {
+        return auth.authorize({password, email})
+            .then(res => {
+                if (!res || res.statusCode === 400 || res.statusCode === 401) throw new Error(`Ошибка: ${res.message}`);
+                if (res.token) {
+                    setIsInfoTooltipPopupOpen(true);
+                    setIsSuccess(true);
+                    setLoggedIn(true);
+                    localStorage.setItem('jwt', res.token);
+                }
+            })
+            .then(tokenCheck)
+            .catch(err => {
+                setIsInfoTooltipPopupOpen(true);
+                setIsSuccess(false);
+                return err;
+            })
+    }
+
+    const handleSignOut = () => {
+        localStorage.removeItem('jwt');
+        setData(initialData);
+        setLoggedIn(false);
+        history.push('/sign-in');
+    }
+
     return (
         <div className="page">
             <CurrentUserContext.Provider value={currentUser}>
                 <Header/>
-                <Main
-                    onEditProfile={handleEditProfileClick}
-                    onAddPlace={handleAddPlaceClick}
-                    onEditAvatar={handleEditAvatarClick}
-                    onCardClick={handleCardClick}
-                    cards={cards}
-                    onCardDelete={handleCardDelete}
-                    onCardLike={handleCardLike}
-                />
+                <Routes>
+                    <ProtectedRoute
+                        path="/mesto"
+                        loggedIn={loggedIn}
+                        isChecking={isAuthChecking}
+                        exact>
+                        <Main
+                            onEditProfile={handleEditProfileClick}
+                            onAddPlace={handleAddPlaceClick}
+                            onEditAvatar={handleEditAvatarClick}
+                            onCardClick={handleCardClick}
+                            cards={cards}
+                            onCardDelete={handleCardDelete}
+                            onCardLike={handleCardLike}
+                        />
+                    </ProtectedRoute>
+                    <Route path='/sign-in' exact>
+                        <Login onLogin={handleLogin}/>
+                    </Route>
+
+                    <Route path='/sign-up' exact>
+                        <Register onRegister={handleRegister}/>
+                    </Route>
+
+                    <Route path="*">
+                        {loggedIn
+                            ? <Navigate to="/mesto"/>
+                            : <Navigate to="/sign-in"/>}
+                    </Route>
+                </Routes>
                 <Footer/>
+
                 <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}
                                   onUpdateUser={handleUpdateUser}/>
                 <AddPlacePopup isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} onAddPlace={handleAddPlaceSubmit}/>
@@ -128,6 +237,13 @@ function App() {
                 <EditAvatarPopup isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}
                                  onUpdateAvatar={handleUpdateAvatar}/>
                 <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+
+                <InfoTooltip
+                    isOpen={isInfoTooltipPopupOpen}
+                    onClose={closePopup}
+                    isSuccess={isSuccess}
+                />
+
             </CurrentUserContext.Provider>
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,9 @@ function Header () {
     return (
       <header className="header">
       <img className="header__logo" src={logo} alt="Место Россия"/>
+          <button type="button"  className="header__button">
+              <span></span>Войти
+          </button>
     </header>             
     );
 }
